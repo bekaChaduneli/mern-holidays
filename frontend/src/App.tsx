5b55bee732299f3e85dc7d8aaa59a0152a8d8435
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-02-03 00:50:23 +0400

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,58 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import Layout from "./layout/Layout";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home page</p>
+            </Layout>
+          }
+        >
+          {/* Nested Routes */}
+          <Route index element={<Home />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <SignIn />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const Home = () => {
+  return <div>Home Page</div>;
+};
+
+const SearchPage = () => {
+  return <div>Search Page</div>;
+};
+
+export default App;
