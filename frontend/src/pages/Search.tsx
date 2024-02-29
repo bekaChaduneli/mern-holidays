2fe5efcdd1b6a89213bd8c16eec2cdde9cd9761f
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2024-02-29 16:15:09 +0400

    implement pagination

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -3,6 +3,7 @@ import { useSearchContext } from "../context/SearchContext";
 import * as apiClient from "../api-client";
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard.";
+import Pagination from "../components/Pagination";
 
 const Search = () => {
   const search = useSearchContext();
@@ -21,6 +22,7 @@ const Search = () => {
     apiClient.searchHotels(searchParams)
   );
 
+  console.log(hotelData);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div className="rounded-lg border border-slate-300 p-5 h-fit lg:sticky top-10">
@@ -42,6 +44,13 @@ const Search = () => {
         {hotelData?.data.map((hotel) => (
           <SearchResultsCard hotel={hotel} />
         ))}
+        <div>
+          <Pagination
+            page={hotelData?.pagination.page || 1}
+            pages={hotelData?.pagination.pages || 1}
+            onPageChange={(page) => setPage(page)}
+          />
+        </div>
       </div>
     </div>
   );
